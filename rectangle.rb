f61b1235975e23fa05a6d2b55206ecaf82dbaeef
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-10-29 11:26:38 -0600

    Add center and draw for stroke and fill

--- rectangle.rb
+++ rectangle.rb
@@ -3,22 +3,24 @@ require_relative "line"
 
 class Rectangle < Shape
 
-	def initialize(x, y, w, h, color)
+	def initialize(x, y, w, h, color, color2)
+		super(w, h)
 		@x = x
 		@y = y
 		@w = w
 		@h = h
 		@color = color
-		@lines = []
-		@lines.push(create_left)
-		@lines.push(create_right)
-		@lines.push(create_top)
-		@lines.push(create_bottom)
-		add_lines(@lines)
+		@color2 = color2
+		add_line(create_left)
+		add_line(create_right)
+		add_line(create_top)
+		add_line(create_bottom)
+		add_center(create_center)
 	end
 
 	def draw
-		draw
+		draw_fill
+		draw_stroke
 	end
 
 	def create_right
@@ -30,11 +32,19 @@ class Rectangle < Shape
 	end
 
 	def create_top
-		Line.new(@x, @y, @x + @w, @y, @color)
+		Line.new(@x - 1, @y, @x + @w, @y, @color)
 	end
 
 	def create_bottom
 		Line.new(@x, @y + @h, @x + @w, @y + @h, @color)
+	end
+
+	def create_center
+		center = []
+		(1...@w).each do |n|
+			center.push(Line.new(@x + n, @y, @x + n, @y + @h - 1, @color2))
+		end
+		center
 	end
 
 end
