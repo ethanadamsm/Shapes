84e4a4af304766930c0010fa21ebdb0c21220984
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-10-29 11:27:51 -0600

    Add test shapes

--- main.rb
+++ main.rb
@@ -1,16 +1,22 @@
 require "gosu"
 require_relative "square"
+require_relative "rectangle"
+require_relative "triangle"
 
 class Main < Gosu::Window
 
 	def initialize
 		super(640, 480)
 
-		@square = Square.new(10, 10, 20, Gosu::Color.argb(0xff_ff0000))
+		@square = Square.new(100, 200, 50, Gosu::Color.argb(0xff_ff0000), Gosu::Color.argb(0xff_ff6600))
+		@rectangle = Rectangle.new(300, 400, 50, 60, Gosu::Color.argb(0xff_ff0000), Gosu::Color.argb(0xff_ff6600))
+		@triangle = Triangle.new(500, 20, 100, 100, Gosu::Color.argb(0xff_ff6600))
 	end
 
 	def draw
 		@square.draw
+		@rectangle.draw
+		@triangle.draw
 	end
 
 end
